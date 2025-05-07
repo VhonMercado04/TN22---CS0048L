1a88deb74f2a17b29b5640e52dc86a3fa346867a
VhonMercado04 <202312243@fit.edu.ph>
2025-05-07 18:06:22 +0800

    Update MP3_p5.py

--- MP3_p5.py
+++ MP3_p5.py
@@ -1,24 +1,37 @@
 subj_name_list = []
 subj_grade_list = []
-final_grade = 0
-ave_grade = 0
+
 def Add_grade(name, grade):
     subj_name_list.append(name)
     subj_grade_list.append(grade)
-    print(name,":",grade,"is added to the list")
+    print(f"\n{name} : {grade} is added to the list")
 
 def calculate_grade():
-    global final_grade
-    global ave_grade
-    for grades in subj_grade_list:
-        ave_grade += grades
-
+    
     if len(subj_grade_list) == 0:
         print("\nAdd Subject and Grade First")
+        return
     
-    else:
-        final_grade = (ave_grade/(len(subj_grade_list)))
-        print(final_grade)    
+    ave_grade = 0
+    
+    for grades in subj_grade_list:
+        ave_grade += grades
+    
+    
+    final_grade = (ave_grade/(len(subj_grade_list)))
+    print("\n-----------------")
+    print("List of subjects:")
+    print("-----------------")
+    for index, subj in enumerate(subj_name_list):
+        print("Subject",(index+1),":",subj)
+        
+    print("-------------------")
+    print("Grade per subjects:")
+    print("-------------------")
+    for index, sub_grade in enumerate(subj_grade_list):
+        print("Your Grade in subject",(index+1),":",sub_grade)
+        
+    print(f"\nYour average grade in all subjects: {final_grade:.2f}")    
         
 while True: 
     print("\n==============================")
@@ -30,16 +43,23 @@ while True:
     choice = int(input("Enter your choice: "))
 
     if choice == 3:
-        print("Exiting program... Thank you")
+        print("\nExiting program... Thank you")
         break
-
+       
     if choice == 1: 
-        subject_name = input("Enter subject name: ")
-        subject_grade = int(input("Enter grade: "))
-        Add_grade(subject_name, subject_grade)
+            subject_name = input("Enter subject name: ")
+            
+            while True:
+                subject_grade = int(input("Enter grade: "))
+            
+                if subject_grade >= 0 and subject_grade <= 100: 
+                    Add_grade(subject_name, subject_grade)
+                    break
+                else:
+                    print("Invalid grade!\n")    
 
     elif choice == 2:
         calculate_grade()
 
     else:
-        print("Invalid input")    
+        print("\nInvalid input!")    
